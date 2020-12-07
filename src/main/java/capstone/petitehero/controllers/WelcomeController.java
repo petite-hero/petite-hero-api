c194a23b6c71b706f61d3c0372928618560048e0
Le Duong <duonglam360@gmail.com>
2020-12-07 12:47:26 +0700

    change name TestController to WelcomeController

--- src/main/java/capstone/petitehero/controllers/WelcomeController.java
+++ src/main/java/capstone/petitehero/controllers/WelcomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class WelcomeController {
 
     @RequestMapping(value = "welcome", method = RequestMethod.GET)
     public static String welcomeAPI() {
